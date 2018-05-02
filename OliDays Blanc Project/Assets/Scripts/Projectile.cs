a38a3d2d92296c688b3ce9369e0e1316e87b4478
Chenapanda <hugo.santos@epita.fr>
2018-05-02 19:25:43 +0200

    Projectile
    l'arme peut tirer, et si c'est un "enemy" qui est touché, le projectile ET l'énemi disparaissent

---
+++ OliDays Blanc Project/Assets/Scripts/Projectile.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Vector3 dir, nextDir;
+    float speed, startSpeed = .025f;
+    bool recalling, disappearing;
+    Transform recallTarget;
+    public void Setup(Vector3 _dir)
+    {
+        dir = _dir; //passed in from player
+        speed = startSpeed; //start moving
+    }
+    void FixedUpdate()
+    {
+        Move(); //move the bullet
+        CheckDisappear(); //get rid of bullet after it stops moving
+    }
+    void Move()
+    {
+        Vector3 tempPos = transform.position; //capture current position
+        tempPos += dir * speed * Time.fixedDeltaTime; //find new position
+        transform.position = tempPos; //update position
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            Destroy(other.gameObject);
+            //add an explosion or something
+            //destroy the projectile that just caused the trigger collision
+            Destroy(gameObject);
+        }
+        print("colision");
+    }
+    void CheckDisappear()
+    {
+        if (speed == 0 && !disappearing)
+        { //disappear and destroy when stopped
+            disappearing = true; //so we dont continuelly call the coroutine
+            StartCoroutine(Disappear());
+        }
+    }
+    IEnumerator Disappear()
+    {
+        float curAlpha = 1; //start at full alpha
+        float disSpeed = 3f; //take 1/3 seconds to disappear
+        do
+        {
+            curAlpha -= disSpeed * Time.deltaTime; //find new alpha
+            yield return null;
+        } while (curAlpha > 0); //end when the bullet is transparent
+        Destroy(gameObject); //get rid of bullet now that it can't be seen
+    }
+}
---
+++ OliDays Blanc Project/Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    bool mouseLeft, canShoot;
+    Vector3 mousePos, mouseVector;
+    public GameObject weapon, weaponTip;
+    public GameObject player;
+    float lastShot = 0, timeBetweenShots = .7f;
+    public GameObject bulletPrefab;
+    // Use this for initialization
+    void Start () {
+        GetMouseInput();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetMouseInput();
+        Animation();
+        Shooting();
+    }
+
+    void GetMouseInput()
+    {
+        //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //position of cursor in world
+        mousePos = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y) - new Vector3(450, 0, 367); ;
+        mousePos.y = player.transform.position.y; //keep the z position consistant, since we're in 2d
+        mouseVector = (mousePos); //normalized vector from player pointing to cursor
+        mouseLeft = Input.GetMouseButton(0); //check left mouse button
+    }
+
+    void Animation()
+    {
+        float gunAngle = -1 * Mathf.Atan2(mouseVector.z, mouseVector.x) * Mathf.Rad2Deg; //find angle in degrees from player to cursor
+        weapon.transform.rotation = Quaternion.AngleAxis(gunAngle, new Vector3 (0, 1, 0)); //rotate gun sprite around that angle
+    }
+    void Shooting()
+    {
+        canShoot = (lastShot + timeBetweenShots < Time.time);
+        if (mouseLeft && canShoot)
+        { //shoot if the mouse button is held and its been enough time since last shot
+            Vector3 spawnPos = weaponTip.transform.position; //position of the tip of the gun, a transform that is a child of rotating gun
+            Quaternion spawnRot = Quaternion.identity; //no rotation, bullets here are round
+            Projectile bul = Instantiate(bulletPrefab, spawnPos, spawnRot).GetComponent<Projectile>();//spawn bullet and capture it's script
+            Vector3 direction = new Vector3(mousePos.z, 0, -mousePos.x);
+            bul.Setup(direction); //give the bullet a direction to fly
+            lastShot = Time.time; //used to check next time this is called
+        }
+    }
+}
